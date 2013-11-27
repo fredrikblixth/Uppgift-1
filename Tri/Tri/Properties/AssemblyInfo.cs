b69ea8acf3a6f01d00ded99cb53870bd367bb78c
fredrikblixth <fb222fn@student.lnu.se>
2013-11-27 18:17:29 +0100

    Enhetstester

---
+++ Tri/Tri/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tri")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Visma Spcs AB")]
+[assembly: AssemblyProduct("Tri")]
+[assembly: AssemblyCopyright("Copyright © Visma Spcs AB 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5068013d-9796-4e9b-85f9-6c3b59fd1f9e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tri/TriangleUnitTest/TriangleTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriangleUnitTest
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void isIsosceles()
+        {
+            // Isosceles triangle
+            var triangle1 = new Triangle(3.0, 6.0, 3.0);
+
+            // Not isosceles triangle
+            var triangle2 = new Triangle(3.0, 4.0, 5.0);
+
+            Assert.IsTrue(triangle1.isIsosceles());
+            Assert.IsFalse(triangle2.isIsosceles());
+        }
+
+        [TestMethod]
+        public void isEquilateral()
+        {
+            // Not Equilateral triangle
+            var triangle1 = new Triangle(3.0, 6.0, 3.0);
+
+            // Not Equilateral triangle
+            var triangle2 = new Triangle(3.0, 4.0, 5.0);
+
+            // Equilateral triangle
+            var triangle3 = new Triangle(3.0, 3.0, 3.0);
+
+            Assert.IsFalse(triangle1.isEquilateral());
+            Assert.IsFalse(triangle2.isEquilateral());
+            Assert.IsTrue(triangle3.isEquilateral());
+        }
+
+        [TestMethod]
+        public void isScalene()
+        {
+            // Not scalene triangle
+            var triangle1 = new Triangle(2.0, 2.0, 2.0);
+            var triangle2 = new Triangle(2.0, 3.0, 2.0);
+
+            // Scalene triangle
+            var triangle3 = new Triangle(3.0, 5.0, 10.0);
+
+            Assert.IsFalse(triangle1.isScalene());
+            Assert.IsFalse(triangle2.isScalene());
+            Assert.IsTrue(triangle3.isScalene());
+        }
+
+        [TestMethod]
+        public void Triangle()
+        {
+            // Constructor with 3 doubles
+            var array = new double[3] {10.0,15.5,20.1};
+            var triangle1 = new Triangle(array);
+
+            Assert.IsFalse(triangle1.isEquilateral());
+            Assert.IsTrue(triangle1.isScalene());
+            Assert.IsFalse(triangle1.isIsosceles());
+
+            // Constructor with 3 points
+
+            var point1 = new Point(0, 0);
+            var point2 = new Point(-3, 0);
+            var point3 = new Point(0, 3);
+
+            var triangle2 = new Triangle(point1, point2, point3);
+
+            Assert.IsTrue(triangle2.isIsosceles());
+            Assert.IsFalse(triangle2.isScalene());
+            Assert.IsFalse(triangle2.isEquilateral());
+
+            var pointArray = new Point[3] {point1,point2,point3};
+
+            var triangle3 = new Triangle(pointArray);
+
+            Assert.IsTrue(triangle3.isIsosceles());
+            Assert.IsFalse(triangle3.isScalene());
+            Assert.IsFalse(triangle3.isEquilateral());
+        }
+    }
+}
